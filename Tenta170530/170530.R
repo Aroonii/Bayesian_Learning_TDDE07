b8d08ab0a5b246f4ff6e44f91727d38b061370c4
Arun Uppugunduri <arun.uppugunduri@gmail.com>
2020-05-27 23:55:12 +0200

    fff

--- Tenta170530/170530.R
+++ Tenta170530/170530.R
@@ -125,13 +125,13 @@ data_dens = bidsCounts/sum(bidsCounts)
 plot(xGrid, data_dens, type = 'l')
 
 #Make repliica draws using draws from posterior
-
+nDraws = 1000
 set.seed(12345)
-theta_draw = rgamma(1000, alpha_posterior, beta_posterior)
+theta_draw = rgamma(nDraws, shape = alpha_posterior, rate = beta_posterior)
 #For each draw theta we want to compute the possion distribution (distribution for
 # x values)
 
-density_mean = rep(0,12)
+density_mean = rep(0, length(xGrid))
 for (i in 1:n){
 #Compute poission distribution over Xgrid = över de x värden vi vill distribuera 
 # proba
@@ -141,13 +141,130 @@ density_mean = density_mean + dpois(xGrid, theta_draw[i])
 #case we compute the mean density for each grid value
 density_mean = density_mean/n
 
-plot(xGrid, data_dens, type = 'l' )
-
-lines(density_mean)
-
+plot(xGrid, density_mean, type = 'o', ylim = c(0,0.2), pch = 'o', xlab = "bids", ylab = "density")
+lines(xGrid, data_dens, col = "red", type = 'b', pch = 'o')
 
 
 
 
+#C
+
+######################## Give Code######################
+###############################
+########## Problem 2 ########## 
+############################### 
+
+# eBay bids data
+load(file = 'bids.RData')    # Loading the vector 'bids' into workspace
+bidsCounts <- table(bids)  # data2Counts is a frequency table of counts.
+xGrid <- seq(min(bids),max(bids))  # A grid used as input to GibbsMixPois.R over which the mixture density is evaluated.
+
+# Code for Problem 3 - Exam in Bayesian Learning 2017-05-30
+GibbsMixPois <- function(x, nComp, alpha, alphaGamma, betaGamma, xGrid, nIter){
+  
+  # Gibbs sampling for a mixture of Poissons
+  # Author: Mattias Villani, IDA, Linkoping University. http://mattiasvillani.com
+  #
+  # INPUTS:
+  #   x - vector with data observations (counts)
+  #   nComp - Number of mixture components to be fitted
+  #   alpha - The prior on the mixture component weights is w ~ Dir(alpha, alpha,..., alpha) 
+  #   alphaGamma and betaGamma - 
+  #              The prior on the mean (theta) of the Poisson mixture components is 
+  #              theta ~ Gamma(alphaGamma, betaGamma) [rate parametrization of the Gamma dist]
+  #   xGrid - the grid of data values over which the mixture is evaluated and plotted
+  #   nIter - Number of Gibbs iterations
+  #
+  # OUTPUTS:
+  #   results$wSample     - Gibbs sample of mixture component weights. nIter-by-nComp matrix
+  #   results$thetaSample - Gibbs sample of mixture component means.   nIter-by-nComp matrix
+  #   results$mixDensMean - Posterior mean of the estimated mixture density over xGrid.
+  
+  
+  ####### Defining a function that simulates from a Dirichlet distribution
+  rDirichlet <- function(param){
+    nCat <- length(param)
+    thetaDraws <- matrix(NA,nCat,1)
+    for (j in 1:nCat){
+      thetaDraws[j] <- rgamma(1,param[j],1)
+    }
+    thetaDraws = thetaDraws/sum(thetaDraws) # Diving every column of ThetaDraws by the sum of the elements in that column.
+    return(thetaDraws)
+  }
+  
+  # Simple function that converts between two different representations of the mixture allocation
+  S2alloc <- function(S){
+    n <- dim(S)[1]
+    alloc <- rep(0,n)
+    for (i in 1:n){
+      alloc[i] <- which(S[i,] == 1)
+    }
+    return(alloc)
+  }
+  
+  # Initial values for the Gibbs sampling
+  nObs <- length(x)
+  S <- t(rmultinom(nObs, size = 1 , prob = rep(1/nComp,nComp))) # nObs-by-nComp matrix with component allocations.
+  theta <- rep(mean(x), nComp) # Each component is initialized at the mean of the data
+  
+  # Setting up the grid where the mixture density is evaluated.
+  mixDensMean <- rep(0,length(xGrid))
+  effIterCount <- 0
+  
+  # Setting up matrices to store the draws
+  wSample <- matrix(0, nIter, nComp)
+  thetaSample <- matrix(0, nIter, nComp)
+  probObsInComp <- rep(NA, nComp)
+  
+  # Setting up the priors - the same prior for all components
+  alpha <- rep(alpha, nComp) 
+  alphaGamma <- rep(alphaGamma, nComp) 
+  betaGamma <- rep(betaGamma, nComp) 
+  
+  # HERE STARTS THE ACTUAL GIBBS SAMPLING
+  
+  for (k in 1:nIter){
+    message(paste('Iteration number:',k))
+    alloc <- S2alloc(S) # Function that converts between different representations of the group allocations
+    nAlloc <- colSums(S)
+    
+    # Step 1 - Update components probabilities
+    w <- rDirichlet(alpha + nAlloc)
+    wSample[k,] <- w
+    
+    # Step 2 - Update theta's in Poisson components
+    for (j in 1:nComp){
+      theta[j] <- rgamma(1, shape = alphaGamma + sum(x[alloc == j]), rate = betaGamma + nAlloc[j])
+    }
+    thetaSample[k,] <- theta
+    
+    # Step 3 - Update allocation
+    for (i in 1:nObs){
+      for (j in 1:nComp){
+        probObsInComp[j] <- w[j]*dpois(x[i], lambda = theta[j])
+      }
+      S[i,] <- t(rmultinom(1, size = 1 , prob = probObsInComp/sum(probObsInComp)))
+    }
+    
+    # Computing the mixture density at the current parameters, and averaging that over draws.
+    effIterCount <- effIterCount + 1
+    mixDens <- rep(0,length(xGrid))
+    for (j in 1:nComp){
+      compDens <- dpois(xGrid, lambda = theta[j])
+      mixDens <- mixDens + w[j]*compDens
+    }
+    mixDensMean <- ((effIterCount-1)*mixDensMean + mixDens)/effIterCount
+  }
+  return(results = list(wSample = wSample, thetaSample = thetaSample, mixDensMean = mixDensMean))
+}
+##################### Given COde#####################################
 
 
+p = GibbsMixPois(bids, 2, alpha, alpha, beta, xGrid, 500)
+p$mixDensMean
+lines(xGrid, p$mixDensMean)
+
+p = GibbsMixPois(bids, 3, alpha, alpha, beta, xGrid, 500)
+p$mixDensMean
+lines(xGrid, p$mixDensMean)
+  
